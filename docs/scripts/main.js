3d4d86af184a4c350a87dc6e9ebad2d9c7e1fc6f
Emmote <emmote2@gmail.com>
2024-09-24 17:05:01 +0100

    Adding sort to Patron Roster.

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,4 +1,5 @@
 const v=1.2
+const patronSort = document.getElementById("patronSort");
 var allExclusivesHidden = true;
 
 function init() {
@@ -6,6 +7,10 @@ function init() {
 	window.addEventListener('hashchange',() =>{
 		swapTab();
 	});
+	
+	if (patronSort!=null&&patronSort!=undefined)
+		patronSort.addEventListener(`change`,sortPatrons);
+	
 	swapTab();
 }
 
@@ -64,6 +69,33 @@ function exclusiveToggleAllContents() {
 	for (let ele of eles) {
 		if (ele.innerHTML==`[${hide} contents]`)
 			ele.innerHTML=`[${show} contents]`;
+	}
+}
+
+function sortPatrons() {
+	let optGroup = document.querySelector('#patronSort option:checked').parentElement.label;
+	let value = patronSort.value;
+	let asc = false;
+	if (optGroup == `Ascending`)
+		asc = true;
+	let eles = document.querySelectorAll('[data-sort]');
+	for (let ele of eles) {
+		let split = ele.dataset.sort.split(",");
+		let index = 0;
+		switch (value) {
+			case "name": index=1; break;
+			case "seat": index=2; break;
+			case "mirt": index=3; break;
+			case "vajra": index=4; break;
+			case "strahd": index=5; break;
+			case "zariel": index=6; break;
+			case "elminster": index=7; break;
+			default: index=0;
+		}
+		let order = split[index];
+		if (!asc)
+			order = -order;
+		ele.style.order = `${order}`;
 	}
 }
 
